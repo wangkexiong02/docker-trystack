9a3a2bcfbefc89cc81e0c99bd6ce9188ac98cf14
wangkexiong <wangkexiong@gmail.com>
2017-04-21 15:50:41 +0800

    Add back nova delete

--- startup.sh
+++ startup.sh
@@ -15,10 +15,33 @@ clouds:
   trystack2:
     auth:
       auth_url: http://8.43.86.2:5000/v2.0
-      password: $TRYSTACK_USER2
-      username: $TRYSTACK_PWD2
+      password: $TRYSTACK_PWD2
+      username: $TRYSTACK_USER2
       project_name: $TRYSTACK_USER2
 ..." > /root/trystack/clouds.yml
 
+echo "
+export OS_AUTH_URL="http://8.43.86.2:5000/v2.0"
+export OS_PASSWORD="$TRYSTACK_PWD1"
+
+export OS_TENANT_NAME="$TRYSTACK_USER1"
+export OS_PROJECT_NAME="$TRYSTACK_USER1"
+export OS_USERNAME="$TRYSTACK_USER1"
+
+export OS_AUTH_STRATEGY=keystone
+" > /root/keystone_trystack1
+
+echo "
+export OS_AUTH_URL="http://8.43.86.2:5000/v2.0"
+export OS_PASSWORD="$TRYSTACK_PWD2"
+
+export OS_TENANT_NAME="$TRYSTACK_USER2"
+export OS_PROJECT_NAME="$TRYSTACK_USER2"
+export OS_USERNAME="$TRYSTACK_USER2"
+
+export OS_AUTH_STRATEGY=keystone
+" > /root/keystone_trystack2
+
 python -m SimpleHTTPServer 80 &
 crond -f -d 8
+
